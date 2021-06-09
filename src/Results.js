8c70c3f63afbfbbca4e6714766e1844ac8e3cf8f
kierab <78439567+kierabon@users.noreply.github.com>
2021-06-09 13:13:44 -0500

    Added  phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic"
 
 export default function Results(props){
    
@@ -9,7 +10,11 @@ export default function Results(props){
     return(
         <div className="Results">
             <h2>{word.toUpperCase()}</h2>
-            
+            {props.results.phonetics.map(function(phonitic, index){
+                return( <div key={index}> 
+                <Phonetic phonetic={phonitic} />
+                </div>)
+            })}
             {props.results.meanings.map (function (meaning,index) {
                 return(
                     <div key={index}>
